fbe7cfba8be96b06322f43a7517aae97efb65f5e
Redul94 <105600346+Redul94@users.noreply.github.com>
2022-11-11 06:26:37 +0600

    Creating map by literal

--- practise_7.dart
+++ practise_7.dart
@@ -1,7 +1,13 @@
+//Creating map by literal
 void main() {
   var mymap = {
     "Name": "Redul",
     "Age": "28",
   };
+  //add new vaule
+  mymap["son"] = "Roman";
+  mymap["sonage"] = "13 month";
   print(mymap);
+  //print one vaule
+  print(mymap["sonage"]);
 }
