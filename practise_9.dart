a4082981d4362801c1db90b5b29cb714437eb135
Redul94 <105600346+Redul94@users.noreply.github.com>
2022-11-11 08:28:43 +0600

    Set in dart

---
+++ practise_9.dart
@@ -0,0 +1,12 @@
+void main() {
+  var myset = new Set.from([10, 20, 39]);
+  //myset.add(10);
+  //myset.add(100);
+  //myset.add(105);
+  //myset.add(106);
+  print(myset);
+  //var my;
+  for (var my in myset) {
+    print(my);
+  }
+}
